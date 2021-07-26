9bef8729ae36808aab831917011e06a0d51f43ee
abhinav-deep <abhinavdeep.dev@gmail.com>
2021-07-26 23:50:27 +0530

    day036

---
+++ Deep/036.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/richest-customer-wealth/
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+       int m=0;
+        for(auto &i : accounts){
+            m = max(m, accumulate(i.begin(),i.end(), 0));
+        }
+        return m;
+    }
+};
