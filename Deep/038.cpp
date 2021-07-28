453e0ecc3b092f88b321548fd08ac24c1f506478
abhinav-deep <abhinavdeep.dev@gmail.com>
2021-07-28 23:33:28 +0530

    day038

---
+++ Deep/038.cpp
@@ -0,0 +1,26 @@
+/*
+Problem - https://codeforces.com/problemset/problem/1482/A
+Author - Abhinav Deep
+Date - July 28, 2021
+*/
+
+#include<iostream>
+using namespace std;
+typedef long long ll;
+void solve();
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+    return;
+}
